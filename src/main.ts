99b76b27d4f155267288d9165814a753501ad7df
xerox0213 <nasreddine021@gmail.com>
2023-10-26 17:26:34 +0200

    fix: correction bug affichage des nombres et opérateurs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 const calculatorBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll("button");
 const calculationResult: HTMLInputElement = document.querySelector("#result") as HTMLInputElement
-
+const operators = ["+", "-", "/", "*"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 calculatorBtns.forEach((btn: HTMLButtonElement) => {
     btn.addEventListener("click", handleClickCalculatorBtn)
 })
@@ -8,39 +9,39 @@ calculatorBtns.forEach((btn: HTMLButtonElement) => {
 function handleClickCalculatorBtn(e: MouseEvent) {
     const calculatorBtn: HTMLButtonElement = e.target as HTMLButtonElement
     const btnValue: string = calculatorBtn.value
-    const operators = ["+", "-", "/", "*"]
-    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     if (operators.includes(btnValue)) {
         displayOperator(btnValue)
     } else if (numbers.includes(btnValue)) {
         displayNumber(btnValue)
     } else if (btnValue === "CE") {
         clearEntry();
+    } else {
+        return;
     }
-    return;
 }
 
 function displayOperator(o: string) {
-    const currentValue = calculationResult.value
-    const lastCharacter: string = getLastCharacter(currentValue)
-    if (lastCharacter === "+" || lastCharacter === "-" || lastCharacter === "/" || lastCharacter === "*") {
-        calculationResult.value = calculationResult.value.substring(0, currentValue.length - 1) + o
+    const lastCharacter: string = getLastCharacter(calculationResult.value)
+    const operands: string[] = extractOperands(calculationResult.value)
+    const lastOperand: string = operands[operands.length - 1]
+    if (operators.includes(lastCharacter) || (o === "-" && operands.length === 1 && lastOperand === "0")) {
+        calculationResult.value = calculationResult.value.slice(0,-1) + o
         return;
     }
     calculationResult.value += o
 }
 
 function displayNumber(n: string) {
-    const currentValue = calculationResult.value
-    const lastCharacter: string = getLastCharacter(currentValue)
-    if (n === "0" && lastCharacter === "0") {
+    const operands: string[] = extractOperands(calculationResult.value)
+    const lastOperand: string = operands[operands.length - 1]
+    if (n === "0" && lastOperand.length === 1 && lastOperand === "0") {
         return;
     }
-    if (lastCharacter === "0" && currentValue.length === 1) {
-        calculationResult.value = n
+    if (n !== "0" && lastOperand.length === 1 && lastOperand === "0") {
+        calculationResult.value = calculationResult.value.slice(0,-1) + n;
         return;
     }
-    calculationResult.value += n
+    calculationResult.value += n;
 }
 
 function getLastCharacter(str: string): string {
@@ -49,4 +50,8 @@ function getLastCharacter(str: string): string {
 
 function clearEntry() {
     calculationResult.value = "0"
+}
+
+function extractOperands(c: string) {
+    return c.split(/[\-\+\/\*]/)
 }
